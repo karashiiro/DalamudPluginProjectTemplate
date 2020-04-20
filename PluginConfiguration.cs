7f84316322cc429123a55b8998dcc5f24c202492
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-04-20 12:32:03 -0700

    Add project files.

---
+++ PluginConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Dalamud.Configuration;
+
+namespace DalamudPluginProjectTemplate
+{
+    public class PluginConfiguration : IPluginConfiguration
+    {
+        public int Version { get; set; }
+
+        // Add any other properties or methods here.
+    }
+}
